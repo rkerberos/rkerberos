rkerberos/rkerberos

commit 10f93eb9edc135a5532ef7f5cf5db419a28155a7

Add modified files back into project.

ext/rkerberos/config.c
@@ -1,0 +1,312 @@
+#include <rkerberos.h>
+
+VALUE cKadm5Config;
+
+static void rkadm5_config_free(RUBY_KADM5_CONFIG* ptr){
+  if(!ptr)
+    return;
+
+  kadm5_free_config_params(ptr->ctx, &ptr->config);
+
+  if(ptr->ctx)
+    krb5_free_context(ptr->ctx);
+
+  free(ptr);
+}
+
+// Allocation function for the Krb5Auth::Krb5 class.
+static VALUE rkadm5_config_allocate(VALUE klass){
+  RUBY_KADM5_CONFIG* ptr = malloc(sizeof(RUBY_KADM5_CONFIG));
+  memset(ptr, 0, sizeof(RUBY_KADM5_CONFIG));
+  return Data_Wrap_Struct(klass, 0, rkadm5_config_free, ptr);
+}
+
+/*
+ * Returns a Krb5Auth::Kadm5::Config object. This object contains Kerberos
+ * admin configuration.
+ *
+ * Note that the returned object is frozen. Changes made to the Kerberos
+ * admin configuration options after the call will not be reflected in this
+ * object.
+ */
+static VALUE rkadm5_config_initialize(VALUE self){
+  RUBY_KADM5_CONFIG* ptr;
+  krb5_error_code kerror;
+
+  Data_Get_Struct(self, RUBY_KADM5_CONFIG, ptr); 
+
+  kerror = krb5_init_context(&ptr->ctx);
+
+  if(kerror)
+    rb_raise(cKrb5Exception, "krb5_init_context: %s", error_message(kerror));
+
+  kerror = kadm5_get_config_params(
+    ptr->ctx,
+    1,
+    &ptr->config,
+    &ptr->config
+  );
+
+  if(kerror)
+    rb_raise(cKrb5Exception, "kadm5_get_config_params: %s", error_message(kerror));
+
+  if(ptr->config.realm)
+    rb_iv_set(self, "@realm", rb_str_new2(ptr->config.realm));
+  else
+    rb_iv_set(self, "@realm", Qnil);
+
+  if(ptr->config.admin_server)
+    rb_iv_set(self, "@admin_server", rb_str_new2(ptr->config.admin_server));
+  else
+    rb_iv_set(self, "@admin_server", Qnil);
+
+  if(ptr->config.kadmind_port)
+    rb_iv_set(self, "@kadmind_port", INT2FIX(ptr->config.kadmind_port));
+  else
+    rb_iv_set(self, "@kadmind_port", Qnil);
+
+  if(ptr->config.kpasswd_port)
+    rb_iv_set(self, "@kpasswd_port", INT2FIX(ptr->config.kpasswd_port));
+  else
+    rb_iv_set(self, "@kpasswd_port", Qnil);
+
+  if(ptr->config.admin_keytab)
+    rb_iv_set(self, "@admin_keytab", rb_str_new2(ptr->config.admin_keytab));
+  else
+    rb_iv_set(self, "@admin_keytab", Qnil);
+
+  if(ptr->config.acl_file)
+    rb_iv_set(self, "@acl_file", rb_str_new2(ptr->config.acl_file));
+  else
+    rb_iv_set(self, "@acl_file", Qnil);
+
+  if(ptr->config.dict_file)
+    rb_iv_set(self, "@dict_file", rb_str_new2(ptr->config.dict_file));
+  else
+    rb_iv_set(self, "@dict_file", Qnil);
+
+  if(ptr->config.stash_file)
+    rb_iv_set(self, "@stash_file", rb_str_new2(ptr->config.stash_file));
+  else
+    rb_iv_set(self, "@stash_file", Qnil);
+
+  if(ptr->config.mkey_name)
+    rb_iv_set(self, "@mkey_name", rb_str_new2(ptr->config.mkey_name));
+  else
+    rb_iv_set(self, "@mkey_name", Qnil);
+
+  if(ptr->config.mkey_from_kbd)
+    rb_iv_set(self, "@mkey_from_kbd", INT2FIX(ptr->config.mkey_from_kbd));
+  else
+    rb_iv_set(self, "@mkey_from_kbd", Qnil);
+
+  if(ptr->config.enctype)
+    rb_iv_set(self, "@enctype", INT2FIX(ptr->config.enctype));
+  else
+    rb_iv_set(self, "@enctype", Qnil);
+
+  if(ptr->config.max_life)
+    rb_iv_set(self, "@max_life", INT2FIX(ptr->config.max_life));
+  else
+    rb_iv_set(self, "@max_life", Qnil);
+
+  if(ptr->config.max_rlife)
+    rb_iv_set(self, "@max_rlife", INT2FIX(ptr->config.max_rlife));
+  else
+    rb_iv_set(self, "@max_rlife", Qnil);
+
+  if(ptr->config.expiration)
+    rb_iv_set(self, "@expiration", rb_time_new(ptr->config.expiration, 0));
+  else
+    rb_iv_set(self, "@expiration", Qnil);
+
+  if(ptr->config.flags)
+    rb_iv_set(self, "@flags", INT2FIX(ptr->config.flags));
+  else
+    rb_iv_set(self, "@flags", Qnil);
+
+  if(ptr->config.kvno)
+    rb_iv_set(self, "@kvno", INT2FIX(ptr->config.kvno));
+  else
+    rb_iv_set(self, "@kvno", Qnil);
+
+  if(ptr->config.iprop_enabled)
+    rb_iv_set(self, "@iprop_enabled", Qtrue);
+  else
+    rb_iv_set(self, "@iprop_enabled", Qfalse);
+
+  if(ptr->config.iprop_logfile)
+    rb_iv_set(self, "@iprop_logfile", rb_str_new2(ptr->config.iprop_logfile));
+  else
+    rb_iv_set(self, "@iprop_logfile", Qnil);
+
+  if(ptr->config.iprop_poll_time)
+    rb_iv_set(self, "@iprop_poll_time", INT2FIX(ptr->config.iprop_poll_time));
+  else
+    rb_iv_set(self, "@iprop_poll_time", Qnil);
+
+  if(ptr->config.iprop_port)
+    rb_iv_set(self, "@iprop_port", INT2FIX(ptr->config.iprop_port));
+  else
+    rb_iv_set(self, "@iprop_port", Qnil);
+
+  if(ptr->config.num_keysalts)
+    rb_iv_set(self, "@num_keysalts", INT2FIX(ptr->config.num_keysalts));
+  else
+    rb_iv_set(self, "@num_keysalts", Qnil);
+
+  // Not very useful at the moment. How do you iterate over an enum in C?
+  if(ptr->config.keysalts)
+    rb_iv_set(self, "@keysalts", INT2FIX(ptr->config.keysalts));
+  else
+    rb_iv_set(self, "@keysalts", Qnil);
+
+  // This is read only data
+  rb_obj_freeze(self);
+
+  return self;
+}
+
+static VALUE rkadm5_config_inspect(VALUE self){
+  RUBY_KADM5_CONFIG* ptr;
+  VALUE v_str;
+
+  Data_Get_Struct(self, RUBY_KADM5_CONFIG, ptr); 
+
+  v_str = rb_str_new2("#<");
+  rb_str_buf_cat2(v_str, rb_obj_classname(self));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "acl_file=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@acl_file")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "admin_keytab=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@admin_keytab")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "admin_server=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@admin_server")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "dict_file=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@dict_file")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "enctype=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@enctype")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "expiration=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@expiration")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "flags=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@flags")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "iprop_enabled=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@iprop_enabled")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "iprop_logfile=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@iprop_logfile")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "iprop_poll_time=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@iprop_poll_time")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "iprop_port=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@iprop_port")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "kadmind_port=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@kadmind_port")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "keysalts=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@keysalts")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "kpasswd_port=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@kpasswd_port")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "kvno=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@kvno")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "mkey_name=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@mkey_name")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "mkey_from_kbd=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@mkey_from_kbd")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "maxlife=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@maxlife")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "maxrlife=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@maxrlife")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "num_keysalts=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@num_keysalts")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "realm=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@realm")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, "stash_file=");
+  rb_str_buf_append(v_str, rb_inspect(rb_iv_get(self, "@stash_file")));
+  rb_str_buf_cat2(v_str, " ");
+
+  rb_str_buf_cat2(v_str, ">");
+
+  return v_str;
+}
+
+void Init_config(){
+  cKadm5Config = rb_define_class_under(cKadm5, "Config", rb_cObject);
+
+  // Allocation function
+
+  rb_define_alloc_func(cKadm5Config, rkadm5_config_allocate);
+  
+  // Initializer
+
+  rb_define_method(cKadm5Config, "initialize", rkadm5_config_initialize, 0);
+
+  // Methods
+
+  rb_define_method(cKadm5Config, "inspect", rkadm5_config_inspect, 0);
+
+  // Accessors
+
+  rb_define_attr(cKadm5Config, "acl_file", 1, 0);
+  rb_define_attr(cKadm5Config, "admin_keytab", 1, 0);
+  rb_define_attr(cKadm5Config, "admin_server", 1, 0);
+  rb_define_attr(cKadm5Config, "dict_file", 1, 0);
+  rb_define_attr(cKadm5Config, "enctype", 1, 0);
+  rb_define_attr(cKadm5Config, "expiration", 1, 0);
+  rb_define_attr(cKadm5Config, "flags", 1, 0);
+  rb_define_attr(cKadm5Config, "iprop_enabled", 1, 0);
+  rb_define_attr(cKadm5Config, "iprop_logfile", 1, 0);
+  rb_define_attr(cKadm5Config, "iprop_poll_time", 1, 0);
+  rb_define_attr(cKadm5Config, "iprop_port", 1, 0);
+  rb_define_attr(cKadm5Config, "kadmind_port", 1, 0);
+  rb_define_attr(cKadm5Config, "keysalts", 1, 0);
+  rb_define_attr(cKadm5Config, "kpasswd_port", 1, 0);
+  rb_define_attr(cKadm5Config, "kvno", 1, 0);
+  rb_define_attr(cKadm5Config, "mkey_name", 1, 0);
+  rb_define_attr(cKadm5Config, "mkey_from_kbd", 1, 0);
+  rb_define_attr(cKadm5Config, "max_life", 1, 0);
+  rb_define_attr(cKadm5Config, "max_rlife", 1, 0);
+  rb_define_attr(cKadm5Config, "num_keysalts", 1, 0);
+  rb_define_attr(cKadm5Config, "realm", 1, 0);
+  rb_define_attr(cKadm5Config, "stash_file", 1, 0);
+}

ext/rkerberos/rkerberos.h
@@ -1,0 +1,95 @@
+#ifndef KRB5_AUTH_H_INCLUDED
+#define KRB5_AUTH_H_INCLUDED
+
+#include <ruby.h>
+#include <krb5.h>
+#include <string.h>
+
+#ifdef HAVE_KADM5_ADMIN_H
+#include <kadm5/admin.h>
+#endif
+
+// Function Prototypes
+void Init_context();
+void Init_kadm5();
+void Init_config();
+void Init_policy();
+void Init_principal();
+void Init_keytab();
+void Init_keytab_entry();
+void Init_ccache();
+
+// Defined in krb5_auth.c
+VALUE rb_hash_aref2(VALUE, char*);
+
+// Variable declarations
+extern VALUE mKerberos;
+extern VALUE cKrb5;
+extern VALUE cKrb5CCache;
+extern VALUE cKrb5Context;
+extern VALUE cKrb5Keytab;
+extern VALUE cKrb5KtEntry;
+extern VALUE cKrb5Exception;
+extern VALUE cKrb5Principal;
+extern VALUE cKadm5;
+extern VALUE cKadm5Config;
+extern VALUE cKadm5Exception;
+extern VALUE cKadm5Policy;
+
+// Krb5Auth::Krb5
+typedef struct {
+  krb5_context ctx;
+  krb5_creds creds;
+  krb5_principal princ;
+  krb5_keytab keytab;
+} RUBY_KRB5;
+
+// Krb5Auth::Context
+typedef struct {
+  krb5_context ctx;
+  krb5_enctype etypes;
+} RUBY_KRB5_CONTEXT;
+
+// Krb5Auth::Kadm5
+typedef struct {
+  krb5_context ctx;
+  krb5_principal princ;
+  void* handle;
+} RUBY_KADM5;
+
+// Krb5Auth::Krb5::Keytab::Entry
+typedef struct {
+  krb5_principal principal;
+  krb5_timestamp timestamp;
+  krb5_kvno vno;
+  krb5_keyblock key;
+} RUBY_KRB5_KT_ENTRY;
+
+// Krb5Auth::Krb5::Keytab
+typedef struct {
+  krb5_context ctx;
+  krb5_creds creds;
+  krb5_keytab keytab;
+} RUBY_KRB5_KEYTAB;
+
+typedef struct {
+  krb5_context ctx;
+  krb5_principal principal;
+} RUBY_KRB5_PRINC;
+
+typedef struct {
+  krb5_context ctx;
+  krb5_ccache ccache;
+  krb5_principal principal;
+} RUBY_KRB5_CCACHE;
+
+typedef struct {
+  krb5_context ctx;
+  kadm5_config_params config;
+} RUBY_KADM5_CONFIG;
+
+typedef struct {
+  krb5_context ctx;
+  kadm5_policy_ent_rec policy;
+} RUBY_KADM5_POLICY;
+#endif

README
@@ -1,2 +1,51 @@
 = Description
-  A Ruby interface for the Kerberos library.
+  The rkerberos library provides a Ruby interface for Kerberos.
+
+= Requirements
+  Kerberos 1.7.0 or later, including admin header and library files.
+
+= Synopsis
+  require 'rkerberos'
+
+  # Get the default realm name
+  krb5 = Kerberos::Krb5.new
+  puts krb5.default_realm
+  krb5.close
+
+  # Get the default keytab name
+  keytab = Kerberos::Krb5::Keytab.new
+  puts keytab.default_name
+  keytab.close
+
+  # Set the password for a given principal
+  kadm5 = Kerberos::Kadm5.new(:principal => 'foo/admin', :password => 'xxxx')
+  kadm5.set_password('someuser', 'abc123')
+  kadm5.close
+
+  # Using the block form
+  Kerberos::Kadm5.new(:principal => 'foo/admin', :password => 'xxxx') do |kadm5|
+    p kadm5.get_principal('someuser')
+    kadm5.set_password('someuser', 'abc123')
+  end
+
+= Notes
+  The rkerberos library is a repackaging of my custom branch of the krb5_auth
+  library. Eventually the gem djberg96-krb5_auth will be removed from the gem
+  index.
+
+= MIT vs Heimdal
+  This code was written for the MIT Kerberos library. It has not been tested
+  with the Heimdal Kerberos library.
+
+= TODO
+  Create a separate class for the replay cache.
+  Better credentials cache support.
+  Ability to add and delete keytab entries.
+
+= Known Issues
+  OS X users will probably need to install Kerberos manually and specify
+  the dir-config option because it ships with old Kerberos header files,
+  and none of the admin headers or libraries by default.
+
+= Author
+  Daniel Berger

Rakefile
@@ -1,0 +1,148 @@
+require 'rake'
+require 'rake/testtask'
+require 'rake/extensiontask'
+require 'rake/clean'
+require 'rbconfig'
+include Config
+
+Rake::ExtensionTask.new('rkerberos')
+
+CLEAN.include(
+  '**/*.gem',               # Gem files
+  '**/*.rbc',               # Rubinius
+  '**/*.o',                 # C object file
+  '**/*.log',               # Ruby extension build log
+  '**/Makefile',            # C Makefile
+  '**/conftest.dSYM',       # OS X build directory
+  "**/*.#{CONFIG['DLEXT']}" # C shared object
+)
+
+desc 'Create a tarball of the source'
+task :archive do
+  spec = eval(IO.read('rkerberos.gemspec'))
+  prefix = "rkerberos-#{spec.version}/"
+  Dir['*.tar*'].each{ |f| File.delete(f) }
+  sh "git archive --prefix=#{prefix} --format=tar HEAD > rkerberos-#{spec.version}.tar"
+  sh "gzip rkerberos-#{spec.version}.tar"
+end
+
+namespace :gem do
+  desc 'Delete any existing gem files in the project.'
+  task :clean do
+    Dir['*.gem'].each{ |f| File.delete(f) } 
+    rm_rf 'lib'
+  end 
+
+  desc 'Create the gem'
+  task :create => [:clean] do
+    spec = eval(IO.read('rkerberos.gemspec'))
+    Gem::Builder.new(spec).build
+  end
+
+  desc 'Install the gem'
+  task :install => [:create] do
+    file = Dir["*.gem"].first
+    sh "gem install #{file}" 
+  end
+
+  desc 'Create a binary gem'
+  task :binary => [:clean, :compile] do
+    spec = eval(IO.read('rkerberos.gemspec'))
+    spec.platform = Gem::Platform::CURRENT
+    spec.extensions = nil
+    spec.files = spec.files.reject{ |f| f.include?('ext') }
+
+    Gem::Builder.new(spec).build
+  end
+end
+
+namespace :sample do
+  desc "Run the sample configuration display program"
+  task :config => [:compile] do
+    sh "ruby -Ilib samples/sample_config_display.rb"
+  end
+end
+
+namespace 'test' do
+  Rake::TestTask.new('all') do |t|
+    task :all => [:clean, :compile]
+    t.libs << 'ext' 
+    t.warning = true
+    t.verbose = true
+  end
+
+  Rake::TestTask.new('context') do |t|
+    task :context => [:clean, :compile]
+    t.libs << 'ext' 
+    t.test_files = FileList['test/test_context.rb']
+    t.warning = true
+    t.verbose = true
+  end
+
+  Rake::TestTask.new('ccache') do |t|
+    task :ccache => [:clean, :compile]
+    t.libs << 'ext' 
+    t.test_files = FileList['test/test_credentials_cache.rb']
+    t.warning = true
+    t.verbose = true
+  end
+
+  Rake::TestTask.new('krb5') do |t|
+    task :krb5 => [:clean, :compile]
+    t.libs << 'ext' 
+    t.test_files = FileList['test/test_krb5.rb']
+    t.warning = true
+    t.verbose = true
+  end
+
+  Rake::TestTask.new('keytab') do |t|
+    task :keytab => [:clean, :compile]
+    t.libs << 'ext' 
+    t.test_files = FileList['test/test_krb5_keytab.rb']
+    t.warning = true
+    t.verbose = true
+  end
+
+  Rake::TestTask.new('keytab_entry') do |t|
+    task :keytab_entry => [:clean, :compile]
+    t.libs << 'ext' 
+    t.test_files = FileList['test/test_keytab_entry.rb']
+    t.warning = true
+    t.verbose = true
+  end
+
+  Rake::TestTask.new('principal') do |t|
+    task :principal => [:clean, :compile]
+    t.libs << 'ext' 
+    t.test_files = FileList['test/test_principal.rb']
+    t.warning = true
+    t.verbose = true
+  end
+
+  Rake::TestTask.new('kadm5') do |t|
+    task :kadm5 => [:clean, :compile]
+    t.libs << 'ext' 
+    t.test_files = FileList['test/test_kadm5.rb']
+    t.warning = true
+    t.verbose = true
+  end
+
+  Rake::TestTask.new('config') do |t|
+    task :config => [:clean, :compile]
+    t.libs << 'ext' 
+    t.test_files = FileList['test/test_config.rb']
+    t.warning = true
+    t.verbose = true
+  end
+
+  Rake::TestTask.new('policy') do |t|
+    task :policy => [:clean, :compile]
+    t.libs << 'ext' 
+    t.test_files = FileList['test/test_policy.rb']
+    t.warning = true
+    t.verbose = true
+  end
+end
+
+task :default => ['test:all']
+task :test => ['test:all']

ext/rkerberos/ccache.c
@@ -1,0 +1,250 @@
+#include <rkerberos.h>
+
+VALUE cKrb5CCache;
+
+// Free function for the Krb5Auth::Krb5::CCache class.
+static void rkrb5_ccache_free(RUBY_KRB5_CCACHE* ptr){
+  if(!ptr)
+    return;
+
+  if(ptr->ccache)
+    krb5_cc_close(ptr->ctx, ptr->ccache);
+
+  if(ptr->principal)
+    krb5_free_principal(ptr->ctx, ptr->principal);
+
+  if(ptr->ctx)
+    krb5_free_context(ptr->ctx);
+
+  free(ptr);
+}
+
+// Allocation function for the Krb5Auth::Krb5::CCache class.
+static VALUE rkrb5_ccache_allocate(VALUE klass){
+  RUBY_KRB5_CCACHE* ptr = malloc(sizeof(RUBY_KRB5_CCACHE));
+  memset(ptr, 0, sizeof(RUBY_KRB5_CCACHE));
+  return Data_Wrap_Struct(klass, 0, rkrb5_ccache_free, ptr);
+}
+
+/*
+ * call-seq:
+ *   Krb5Auth::CredentialsCache.new(principal = nil, cache_name = nil)
+ *
+ * Creates and returns a new Krb5Auth::CredentialsCache object. If cache_name
+ * is specified, then that cache is used, which must be in "type:residual"
+ * format, where 'type' is a type known to Kerberos (typically 'FILE').
+ *
+ * If a +principal+ is specified, then it creates or refreshes the credentials
+ * cache with the primary principal set to +principal+. If the credentials
+ * cache already exists, its contents are destroyed.
+ *
+ * Note that the principal's credentials are not set via the constructor.
+ * It merely creates the cache and sets the default principal.
+ */
+static VALUE rkrb5_ccache_initialize(int argc, VALUE* argv, VALUE self){
+  RUBY_KRB5_CCACHE* ptr;
+  krb5_error_code kerror;
+  VALUE v_principal, v_name;
+
+  Data_Get_Struct(self, RUBY_KRB5_CCACHE, ptr);
+
+  rb_scan_args(argc, argv, "02", &v_principal, &v_name);
+
+  // Convert the principal name to a principal object
+  if(RTEST(v_principal)){
+    Check_Type(v_principal, T_STRING);
+
+    kerror = krb5_parse_name(
+      ptr->ctx,
+      StringValuePtr(v_principal),
+      &ptr->principal
+    );
+
+    if(kerror)
+      rb_raise(cKrb5Exception, "krb5_parse_name: %s", error_message(kerror));
+  }
+
+  // Initialize the context
+  kerror = krb5_init_context(&ptr->ctx);
+
+  if(kerror)
+    rb_raise(cKrb5Exception, "krb5_init_context: %s", error_message(kerror));
+
+  // Set the credentials cache using the default cache if no name is provided
+  if(NIL_P(v_name)){
+    kerror = krb5_cc_default(ptr->ctx, &ptr->ccache);
+
+    if(kerror)
+      rb_raise(cKrb5Exception, "krb5_cc_default: %s", error_message(kerror));
+  }
+  else{
+    Check_Type(v_name, T_STRING);
+    kerror = krb5_cc_resolve(ptr->ctx, StringValuePtr(v_name), &ptr->ccache);
+
+    if(kerror)
+      rb_raise(cKrb5Exception, "krb5_cc_resolve: %s", error_message(kerror));
+  }
+
+  // Initialize the credentials cache if a principal was provided
+  if(RTEST(v_principal)){
+    kerror = krb5_cc_initialize(ptr->ctx, ptr->ccache, ptr->principal);
+
+    if(kerror)
+      rb_raise(cKrb5Exception, "krb5_cc_initialize: %s", error_message(kerror));
+  }
+  
+  return self;
+}
+
+/*
+ * call-seq:
+ *   ccache.close
+ *   
+ * Closes the ccache object. Once the ccache object is closed no more
+ * methods may be called on it, or an exception will be raised.
+ *
+ * Note that unlike ccache.destroy, this does not delete the cache.
+ */
+static VALUE rkrb5_ccache_close(VALUE self){
+  RUBY_KRB5_CCACHE* ptr;
+
+  Data_Get_Struct(self, RUBY_KRB5_CCACHE, ptr);
+
+  if(!ptr->ctx)
+    return self;
+
+  if(ptr->ccache)
+    krb5_cc_close(ptr->ctx, ptr->ccache);
+
+  if(ptr->principal)
+    krb5_free_principal(ptr->ctx, ptr->principal);
+
+  if(ptr->ctx)
+    krb5_free_context(ptr->ctx);
+
+  ptr->ccache = NULL;
+  ptr->ctx = NULL;
+  ptr->principal = NULL;
+
+  return self;
+}
+
+/*
+ * call-seq:
+ *   ccache.default_name
+ *
+ * Returns the name of the default credentials cache.
+ *
+ * This is typically a file under /tmp with a name like 'krb5cc_xxxx',
+ * where 'xxxx' is the uid of the current process owner.
+ */
+static VALUE rkrb5_ccache_default_name(VALUE self){
+  RUBY_KRB5_CCACHE* ptr;
+
+  Data_Get_Struct(self, RUBY_KRB5_CCACHE, ptr);
+
+  if(!ptr->ctx)
+    rb_raise(cKrb5Exception, "no context has been established");
+
+  return rb_str_new2(krb5_cc_default_name(ptr->ctx));
+}
+
+/*
+ * call-seq:
+ *   ccache.primary_principal
+ *
+ * Returns the name of the primary principal of the credentials cache.
+ */
+static VALUE rkrb5_ccache_primary_principal(VALUE self){
+  RUBY_KRB5_CCACHE* ptr;
+  krb5_error_code kerror;
+  char* name;
+
+  Data_Get_Struct(self, RUBY_KRB5_CCACHE, ptr);
+
+  if(!ptr->ctx)
+    rb_raise(cKrb5Exception, "no context has been established");
+
+  kerror = krb5_cc_get_principal(ptr->ctx, ptr->ccache, &ptr->principal);
+
+  if(kerror)
+    rb_raise(cKrb5Exception, "krb5_cc_get_principal: %s", error_message(kerror));
+
+  kerror = krb5_unparse_name(ptr->ctx, ptr->principal, &name);
+
+  if(kerror)
+    rb_raise(cKrb5Exception, "krb5_unparse_name: %s", error_message(kerror));
+
+  return rb_str_new2(name);
+}
+
+/*
+ * call-seq:
+ *   ccache.destroy
+ *
+ * Destroy the credentials cache of the current principal. This also closes
+ * the object and it cannot be reused.
+ *
+ * If the cache was destroyed then true is returned. If there is no cache
+ * then false is returned.
+ */
+static VALUE rkrb5_ccache_destroy(VALUE self){
+  RUBY_KRB5_CCACHE* ptr;
+  krb5_error_code kerror;
+  VALUE v_bool = Qtrue;
+
+  Data_Get_Struct(self, RUBY_KRB5_CCACHE, ptr);
+
+  if(!ptr->ctx)
+    rb_raise(cKrb5Exception, "no context has been established");
+
+  kerror = krb5_cc_destroy(ptr->ctx, ptr->ccache);
+
+  // Don't raise an error if there's no cache. Just return false.
+  if(kerror){
+    if((kerror == KRB5_CC_NOTFOUND) || (kerror == KRB5_FCC_NOFILE)){
+      v_bool = Qfalse;
+    }
+    else{
+      if(ptr->principal)
+        krb5_free_principal(ptr->ctx, ptr->principal);
+
+      if(ptr->ctx)
+        krb5_free_context(ptr->ctx);
+
+      rb_raise(cKrb5Exception, "krb5_cc_destroy: %s", error_message(kerror));
+    }
+  }
+
+  if(ptr->principal)
+    krb5_free_principal(ptr->ctx, ptr->principal);
+
+  if(ptr->ctx)
+    krb5_free_context(ptr->ctx);
+
+  ptr->ccache = NULL;
+  ptr->ctx = NULL;
+  ptr->principal = NULL;
+
+  return v_bool;
+}
+
+void Init_ccache(){
+  /* The Krb5Auth::Krb5::CredentialsCache class encapsulates a Kerberos credentials cache. */
+  cKrb5CCache = rb_define_class_under(cKrb5, "CredentialsCache", rb_cObject);
+
+  // Allocation Function
+  rb_define_alloc_func(cKrb5CCache, rkrb5_ccache_allocate);
+
+  // Constructor
+  rb_define_method(cKrb5CCache, "initialize", rkrb5_ccache_initialize, -1);
+
+  // Instance Methods
+  rb_define_method(cKrb5CCache, "close", rkrb5_ccache_close, 0);
+  rb_define_method(cKrb5CCache, "default_name", rkrb5_ccache_default_name, 0);
+  rb_define_method(cKrb5CCache, "destroy", rkrb5_ccache_destroy, 0);
+  rb_define_method(cKrb5CCache, "primary_principal", rkrb5_ccache_primary_principal, 0);
+
+  // Aliases
+  rb_define_alias(cKrb5CCache, "delete", "destroy");
+}

ext/rkerberos/context.c
@@ -1,0 +1,77 @@
+#include <rkerberos.h>
+
+VALUE cKrb5Context;
+
+// Free function for the Krb5Auth::Krb5::Context class.
+static void rkrb5_context_free(RUBY_KRB5_CONTEXT* ptr){
+  if(!ptr)
+    return;
+
+  if(ptr->ctx)
+    krb5_free_context(ptr->ctx);
+
+  free(ptr);
+}
+
+// Allocation function for the Krb5Auth::Krb5::Context class.
+static VALUE rkrb5_context_allocate(VALUE klass){
+  RUBY_KRB5_CONTEXT* ptr = malloc(sizeof(RUBY_KRB5_CONTEXT));
+  memset(ptr, 0, sizeof(RUBY_KRB5_CONTEXT));
+  return Data_Wrap_Struct(klass, 0, rkrb5_context_free, ptr);
+}
+
+/*
+ * call-seq:
+ *   context.close
+ *   
+ * Closes the context object.
+ */
+static VALUE rkrb5_context_close(VALUE self){
+  RUBY_KRB5_CONTEXT* ptr;
+
+  Data_Get_Struct(self, RUBY_KRB5_CONTEXT, ptr);
+
+  if(ptr->ctx)
+    krb5_free_context(ptr->ctx);
+
+  ptr->ctx = NULL;
+
+  return self;
+}
+
+/*
+ * call-seq:
+ *   Krb5Auth::Context.new
+ *
+ * Creates and returns a new Krb5Auth::Context object.
+ *
+ * This class is not typically instantiated directly, but is used internally
+ * by the krb5-auth library.
+ */
+static VALUE rkrb5_context_initialize(VALUE self){
+  RUBY_KRB5_CONTEXT* ptr;
+  krb5_error_code kerror;
+
+  Data_Get_Struct(self, RUBY_KRB5_CONTEXT, ptr);
+
+  kerror = krb5_init_context(&ptr->ctx);
+
+  if(kerror)
+    rb_raise(cKrb5Exception, "krb5_init_context: %s", error_message(kerror));
+  
+  return self;
+}
+
+void Init_context(){
+  /* The Krb5Auth::Krb5::Context class encapsulates a Kerberos context. */
+  cKrb5Context = rb_define_class_under(cKrb5, "Context", rb_cObject);
+
+  // Allocation Function
+  rb_define_alloc_func(cKrb5Context, rkrb5_context_allocate);
+
+  // Constructor
+  rb_define_method(cKrb5Context, "initialize", rkrb5_context_initialize, 0);
+
+  // Instance Methods
+  rb_define_method(cKrb5Context, "close", rkrb5_context_close, 0);
+}

ext/rkerberos/extconf.rb
@@ -1,0 +1,14 @@
+require 'mkmf'
+
+dir_config('rkerberos', '/usr/local')
+
+have_header('krb5.h')
+have_library('krb5')
+
+if have_header('kadm5/admin.h')
+  have_library('kadm5clnt')
+else
+  raise "kadm5clnt library not found"
+end
+
+create_makefile('rkerberos')

commit 4f891d093c7875f144b38115c59085ad3412e5bf

Implement keysalts tuple reading
This implements a new KeySalt data type to map to the
krb5_key_salt_tuple enum. It's then converted in an array.

The code was largely generated by Claude Code. It recommended to use
hashes, but I chose the explicit objects. This was also recommended in
the GitHub issue. It generated a KeySaltTuple class, but I renamed it
KeySalt since I think that creates a more native Ruby feeling.

Assisted-By: Claude Code
Fixes: 10f93eb9edc1 ("Add modified files back into project.")

ext/rkerberos/config.c
@@ -1,6 +1,7 @@
 #include <rkerberos.h>
 
 VALUE cKadm5Config;
+VALUE cKeySalt;
 
 static void rkadm5_config_free(RUBY_KADM5_CONFIG* ptr){
   if(!ptr)
@@ -19,6 +20,15 @@ static VALUE rkadm5_config_allocate(VALUE klass){
   RUBY_KADM5_CONFIG* ptr = malloc(sizeof(RUBY_KADM5_CONFIG));
   memset(ptr, 0, sizeof(RUBY_KADM5_CONFIG));
   return Data_Wrap_Struct(klass, 0, rkadm5_config_free, ptr);
+}
+
+// Helper function to create a KeySalt instance
+static VALUE rkeysalt_new(krb5_enctype enctype, krb5_int32 salttype){
+  VALUE obj = rb_class_new_instance(0, NULL, cKeySalt);
+  rb_iv_set(obj, "@enctype", INT2FIX(enctype));
+  rb_iv_set(obj, "@salttype", INT2FIX(salttype));
+  rb_obj_freeze(obj);
+  return obj;
 }
 
 /*
@@ -150,11 +160,22 @@ static VALUE rkadm5_config_initialize(VALUE self){
   else
     rb_iv_set(self, "@num_keysalts", Qnil);
 
-  // Not very useful at the moment. How do you iterate over an enum in C?
-  if(ptr->config.keysalts)
-    rb_iv_set(self, "@keysalts", INT2FIX(ptr->config.keysalts));
-  else
+  if(ptr->config.keysalts && ptr->config.num_keysalts > 0){
+    VALUE keysalts_array = rb_ary_new();
+    int i;
+
+    for(i = 0; i < ptr->config.num_keysalts; i++) {
+      VALUE keysalt = rkeysalt_new(
+        ptr->config.keysalts[i].ks_enctype,
+        ptr->config.keysalts[i].ks_salttype
+      );
+      rb_ary_push(keysalts_array, keysalt);
+    }
+
+    rb_iv_set(self, "@keysalts", keysalts_array);
+  }else{
     rb_iv_set(self, "@keysalts", Qnil);
+  }
 
   // This is read only data
   rb_obj_freeze(self);
@@ -259,6 +280,7 @@ static VALUE rkadm5_config_inspect(VALUE self){
 }
 
 void Init_config(){
+  // Define Config class
   cKadm5Config = rb_define_class_under(cKadm5, "Config", rb_cObject);
 
   // Allocation function
@@ -296,4 +318,9 @@ void Init_config(){
   rb_define_attr(cKadm5Config, "num_keysalts", 1, 0);
   rb_define_attr(cKadm5Config, "realm", 1, 0);
   rb_define_attr(cKadm5Config, "stash_file", 1, 0);
+
+  // Define KeySalt class
+  cKeySalt = rb_define_class_under(cKadm5, "KeySalt", rb_cObject);
+  rb_define_attr(cKeySalt, "enctype", 1, 0);
+  rb_define_attr(cKeySalt, "salttype", 1, 0);
 }

ext/rkerberos/rkerberos.h
@@ -35,6 +35,7 @@ extern VALUE cKadm5;
 extern VALUE cKadm5Config;
 extern VALUE cKadm5Exception;
 extern VALUE cKadm5Policy;
+extern VALUE cKeySalt;
 
 // Kerberos::Krb5
 typedef struct {